fdb5590824ef0670e8b4a45f031a6389e78322f7
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:53:09 +0500

    Mark number
    in this repository w are creating a typescript file of  Mark number

--- app.ts
+++ app.ts
@@ -1,11 +1,16 @@
-import { toASCII } from "punycode";
-
-let laptop = {
-    make: "dell",
-    model: "XPS 15",
-    year: 2021,
-    describe: function() {
-        console.log(`This laptop is a ${this.year} ${this.make} ${this.year}`);
-    }
+let mark: number = 55;
+if (mark >= 90){
+    console.log("Grade 90 A+");
+}else if (mark >= 80){
+    console.log("Grade 80 A");
+} else if (mark >= 70){
+    console.log("Grade 70 B");
+} else if (mark >= 60){
+    console.log("Grade 60 C");
+}else if (mark >= 50){
+    console.log("Grade 50 D");
+}else{
+    console.log("Grade:F");
 }
-laptop.describe()
+
+    
