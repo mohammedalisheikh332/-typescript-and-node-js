62f4f776a43428d30a4273de98d9c8633b066c92
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 20:11:05 +0500

    switch
    in this repository w are creating a typescript file of switch

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+let dayoff = "sunday";
+switch (dayoff) {
+    case "saturday":
+        console.log("Go hiking.");
+        break;
+    case "Sunday":
+        console.log("Read a book.");
+        break;
+    default:
+        console.log("work on a Hobby.");
+}
