055ca2f8b1d3d5a47961f25ed80e14312e62fc42
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:06:35 +0500

    Guestlist
    in this repository w are creating a typescript file of Guestlist

--- app.ts
+++ app.ts
@@ -1,8 +1,5 @@
-let array: (string | number) [] = ['Raza','Ali','Babar',2,3,7 ,"saba"]
-console.log(array [7]);
-console.log(array [0]);
-console.log(array [1]);
-console.log(array [2]);
-console.log(array [3]);
-console.log(array [4]);
-console.log(array [5]);
+let guest_list: string[] = ['Babar','Saim' ,'Rizwan', 'Shaheen Afridi',];
+for(let i=0; i<guest_list.length;i++){
+    console.log('Respected sir'+ guest_list[i] +' ,\n we invited you on dinner tomorrow.\n Thankyou\n')
+}
+export{guest_list}
