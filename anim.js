ef03c6df9fa60fcaa8cd3ce519e7ac80e64d4939
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:26:42 +0500

    Animal
    in this repository w are creating a typescript file of animal

---
+++ anim.js
@@ -0,0 +1,6 @@
+"use strict";
+var animals = ["cow", "rabbit", "pegin"];
+animals.forEach(function (animal) {
+    console.log("A ".concat(animal, " would make a great pet."));
+});
+console.log("Any of these animals would make a great pet!");
