f009349e0cd60f9b40375c2014151d3ad372c703
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:14:33 +0500

    condition Test
    in this repository w are creating a typescript file of Condition Test

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("../GuestList/app");
-console.log("n/printer number of guest invited");
-console.log("we had finally invited ".concat(app_1.guest_list.length, " from exercises 14"));
+var car = "BMW";
+//test1:equality camparison(true)
+console.log("Is car =='BMW'? I predict true");
+console.log(car == 'BMW'); //true
+//test2:equality camparison(true)
+console.log("Is car ==='BMW'? I predict true");
+console.log(car === 'BMW'); //true
+//test3:inequality camparison(flase)
+console.log("Is car !='BMW'? I predict flase");
+console.log(car != 'BMW'); //flase
+//test3:inequality camparison(flase)
+console.log("Is car !=='BMW'? I predict flase");
+console.log(car !== 'BMW'); //flase
+//test5: less then camparison(flase)
+console.log("Is car < 'BMW'? I predict flase");
+console.log(car < 'BMW'); //flase (lexiographic camparison)
+//test6: grater then camparison(flase)
+console.log("Is car > 'BMW'? I predict flase");
+console.log(car > 'BMW'); //flase (lexiographic camparison
+//test7: less then equal camparison(true)
+console.log("Is car <= 'BMW'? I predict true");
+console.log(car <= 'BMW'); //true
+//test8: grater then equal camparison(true)
+console.log("Is car >= 'BMW'? I predict true");
+console.log(car >= 'BMW'); //true
+//test9: checking and truthiness
+console.log("Is car?I predict true");
+console.log(car); // true(non empty is truty)
+//test10: checking and flaseiness
+console.log("Is car?I predict flase");
+console.log(car); // flase(negation of a truthy value)
--- app.ts
+++ app.ts
@@ -1,3 +1,43 @@
-import {guest_list} from '../GuestList/app'
-console.log(`n/printer number of guest invited`)
-console.log(`we had finally invited ${guest_list.length} from exercises 14`);
+ let car: string = "BMW";
+ //test1:equality camparison(true)
+ console.log("Is car =='BMW'? I predict true");
+ console.log(car == 'BMW');//true
+
+ //test2:equality camparison(true)
+ console.log("Is car ==='BMW'? I predict true");
+ console.log(car === 'BMW');//true
+
+ //test3:inequality camparison(flase)
+ console.log("Is car !='BMW'? I predict flase")
+ console.log(car != 'BMW');//flase
+
+ //test3:inequality camparison(flase)
+ console.log("Is car !=='BMW'? I predict flase")
+ console.log(car !== 'BMW');//flase
+
+ //test5: less then camparison(flase)
+ console.log("Is car < 'BMW'? I predict flase")
+ console.log(car < 'BMW');//flase (lexiographic camparison)
+
+ //test6: grater then camparison(flase)
+ console.log("Is car > 'BMW'? I predict flase")
+ console.log(car  > 'BMW');//flase (lexiographic camparison
+
+
+ //test7: less then equal camparison(true)
+ console.log("Is car <= 'BMW'? I predict true");
+ console.log(car <= 'BMW');//true
+
+ //test8: grater then equal camparison(true)
+ console.log("Is car >= 'BMW'? I predict true");
+ console.log(car >= 'BMW');//true
+
+ //test9: checking and truthiness
+ console.log("Is car?I predict true");
+ console.log(car);// true(non empty is truty)
+
+ //test10: checking and flaseiness
+ console.log("Is car?I predict flase");
+ console.log(car);// flase(negation of a truthy value)
+
+
