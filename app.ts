559d9b47d6458d32011593e4995bc21934057133
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:04:12 +0500

    Member name
    in this repository w are creating a typescript file of Member Name

--- app.ts
+++ app.ts
@@ -1,15 +1,4 @@
-let guest_list: string[] = ['Babar','Saim' ,'Rizwan', 'Shaheen Afridi',];
-for(let i=0; i<guest_list.length;i++){
-    console.log('Respected sir'+ guest_list[i] +' ,\n we invited you on dinner tomorrow.\n Thankyou\n')
-}
-let not_present: string = 'Riwzan';
-let new_guest: string = 'Hania Amir';
-guest_list[2] = new_guest;
-for(let i=0; i<guest_list.length; i++){
-    console.log('Respected sir/madam' + guest_list[i] + ',\n We invited you on dinner tomorrow.\n Thankyou\n')
-}
-console.log(`Mr. ${not_present} will not coming to dinner tomorrow.`);
-guest_list.unshift('Mahira khan' , 'Kinza', 'maya');
-for(let i=0; i<guest_list.length; i++){
-    console.log('Respected sir/madam ' + guest_list[i] + ',\n We invited you on dinner tomorrow,Because we have a bigger table now.\n Thankyou\n')
+let members: string[] =['ali','raza','saba','anas','hina'];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
 }
