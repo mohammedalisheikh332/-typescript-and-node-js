7ca62d674cf8607e9d65312068ce594942ca56dc
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:43:44 +0500

    magicians
    in this repository w are creating a typescript file of magicians

---
+++ mag.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ["Mohammed", "Asif", "former pakistani fast blower"];
+function show_magicians(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ mag.ts
@@ -0,0 +1,10 @@
+let magicians: string[] = ["Mohammed", "Asif", "former pakistani fast blower"]
+
+function show_magicians(magicians:string[]) {
+    magicians.forEach(magician => 
+        {
+            console.log(magician);
+        })
+}
+
+show_magicians(magicians)
