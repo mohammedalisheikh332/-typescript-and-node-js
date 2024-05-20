5c59222bf0223af4a855c15d69e606e583f96d9d
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:16:03 +0500

    chaning Guest_list
    in this repository w are creating a typescript file of chaning Guest-list

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-var car = "BMW";
-//test1:equality camparison(true)
-console.log("Is car =='BMW'? I predict true");
-console.log(car == 'BMW'); //true
-//test2:equality camparison(true)
-console.log("Is car ==='BMW'? I predict true");
-console.log(car === 'BMW'); //true
-//test3:inequality camparison(flase)
-console.log("Is car !='BMW'? I predict flase");
-console.log(car != 'BMW'); //flase
-//test3:inequality camparison(flase)
-console.log("Is car !=='BMW'? I predict flase");
-console.log(car !== 'BMW'); //flase
-//test5: less then camparison(flase)
-console.log("Is car < 'BMW'? I predict flase");
-console.log(car < 'BMW'); //flase (lexiographic camparison)
-//test6: grater then camparison(flase)
-console.log("Is car > 'BMW'? I predict flase");
-console.log(car > 'BMW'); //flase (lexiographic camparison
-//test7: less then equal camparison(true)
-console.log("Is car <= 'BMW'? I predict true");
-console.log(car <= 'BMW'); //true
-//test8: grater then equal camparison(true)
-console.log("Is car >= 'BMW'? I predict true");
-console.log(car >= 'BMW'); //true
-//test9: checking and truthiness
-console.log("Is car?I predict true");
-console.log(car); // true(non empty is truty)
-//test10: checking and flaseiness
-console.log("Is car?I predict flase");
-console.log(car); // flase(negation of a truthy value)
+var guest_list = ['Babar', 'Saim', 'Rizwan', 'Shaheen Afridi',];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir' + guest_list[i] + ' ,\n we invited you on dinner tomorrow.\n Thankyou\n');
+}
+var not_present = 'Riwzan';
+var new_guest = 'Hania Amir';
+guest_list[2] = new_guest;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/madam' + guest_list[i] + ',\n We invited you on dinner tomorrow.\n Thankyou\n');
+}
+console.log("Mr. ".concat(not_present, " will not coming to dinner tomorrow."));
