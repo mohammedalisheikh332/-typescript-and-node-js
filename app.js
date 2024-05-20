a7e99b3e316d2947afe45048a02bc6b716d1f331
Ali sheikh <167357338+mohammedalisheikh332@users.noreply.github.com>
2024-05-20 16:22:06 +0500

    Adding Coming
    in this repository w are creating a typescript file of Adding coming

--- app.js
+++ app.js
@@ -1,47 +1,5 @@
-var alien_color = "green";
-if (alien_color == "green") {
-    console.log(" the player just eraned the 5 point ");
-}
-// if the alien yellow,print a message that the player earned the 10 point
-else if (alien_color == "yellow") {
-    console.log("the player just earned 10 point.");
-}
-// if the alien red,print a message that the player earned the 15 point
-else if (alien_color == "red") {
-    console.log("the player just earned 15 point.");
-}
-else {
-    console.log("please select the right color");
-}
-// write  three version of this program, making sure 
-// each message is printed apporpriate color alien
-// version 2.
-alien_color = "red";
-if (alien_color == "green") {
-    console.log("the player earned just 5 point");
-}
-alien_color = "Yellow";
-if (alien_color == "yellow") {
-    console.log("the player earned just 10 point");
-}
-if (alien_color == "red") {
-    console.log("the player earned just 15 point");
-}
-else {
-    console.log("please select the right color");
-}
-// version 3.
-alien_color = "red";
-if (alien_color == "green") {
-    console.log("the player earned just 5 point");
-}
-alien_color = "Yellow";
-if (alien_color == "yellow") {
-    console.log("the player earned just 10 point");
-}
-if (alien_color == "red") {
-    console.log("the player earned just 15 point");
-}
-else {
-    console.log("please select the right color");
-}
+//exerise
+//MyName is Ali
+//Dated 1-3-2024
+//This program will run simple code like Hello world.
+console.log('Hello world simple Program');
---
+++ program2.ts
@@ -0,0 +1,5 @@
+//exerise
+//MyName is Ali
+//Dated 1-3-2024
+//This program will multiple.
+console.log(5*10);
